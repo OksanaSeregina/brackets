71b2b8f2b99c79a27fb4697185c171ab53331664
Oksana <seregina.oksana.1987@gmail.com>
2022-02-22 11:01:27 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) {
+    return false;
+  }
+  let array = [];
+  let newString = str;
+  for (let item of bracketsConfig) {
+    array.push(item.join(""));
+  }
+  for (let i = 0; i <= str.length / 2; i++) {
+    for (let item of array) {
+      newString = newString.replace(item, "");
+    }
+  }
+  return newString.length === 0;
+};
